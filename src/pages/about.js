9c1ab21faf98449f068c6ec242edae3218552890
岩本 <k_iwamoto@stride-web.co.jp>
2024-11-27 15:02:25 +0900

    commit

--- src/pages/about.js
+++ src/pages/about.js
@@ -35,7 +35,7 @@ return (
             <div className={styles.profileContent}>
                 <h3>岩本 謙吾<span>KENGO IWAMOTO</span></h3>
                 <p>はじめまして、岩本謙吾です。<br />
-                    WEB制作との出会いは19歳の頃、ブログでのCSS装飾がきっかけでした。独学でHTML、CSSを学び、その後はセブ島のIT留学でWEBデザインコースを受講しました。IT留学後は都内のWEB制作会社でフロントエンドエンジニアとしてお仕事をしています。コーディングもデザインもどっちも好き。デザインもコーディングもどっちも極めたい。<br />最近はGSAPのアニメーションが好きです。このサイトではscrollTriggerを使っています。</p>
+                    WEB制作との出会いは19歳の頃、ブログでのCSS装飾がきっかけでした。独学でHTML、CSSを学び、その後はセブ島のIT留学でWEBデザインコースを受講しました。IT留学後は都内のWEB制作会社でフロントエンドエンジニアとしてお仕事をしています。コーディングもデザインもどっちも好き。最近はPhotoshopやIllustratorでのバナー作成の勉強をしています。</p>
             </div>
 
             <div className={styles.service}>
@@ -43,21 +43,21 @@ return (
                     <div className={styles.serviceNumber}>1</div>
                     <div className={styles.serviceContent}>
                         <h3>WEBデザイン</h3>
-                        <p>企業や個人のオンライン上の顔となるWEBデザインを作っています。次につながるデザイン、新たなつながりを作るデザイン、成果の出るデザインをモットーに作成しています。かっちりしたデザインが好みですが、デザインに偏りが出ないように様々なデザインに触れています。バナー作成も猛烈勉強中。</p>
+                        <p>プログラミングスクールのWEBデザインコースを受講し、PhotoshopやAdobe XDのデザインツールを学びました。現在はWEBディレクション業務でXDやFigmaを使ってワイヤーフレームの作成を行っています。</p>
                     </div>
                 </div>
                 <div className={styles.serviceRow}>
                     <div className={styles.serviceNumber}>2</div>
                     <div className={styles.serviceContent}>
                         <h3>コーディング</h3>
-                        <p>きれいなコーディング、再利用しやすいコーディング、SEOに有利なコーディング、デザインに忠実なコーディングを意識しています。Javascriptを使用したリッチなアニメーションもお任せください。WEB制作領域においてjQueryのシェアは未だに高いですが、次世代のWEB開発をできるReactも可能です。</p>
+                        <p>きれいなコーディング、再利用しやすいコーディング、SEOに有利なコーディング、デザインに忠実なコーディングを意識しています。フロントエンドのコーディングがメインですが、PHPやLaravelなどのバックエンドも経験があります。最近はNext.jsを勉強していて、本ポートフォリオもNext.jsを使用しています。</p>
                     </div>
                 </div>
                 <div className={styles.serviceRow}>
                     <div className={styles.serviceNumber}>3</div>
                     <div className={styles.serviceContent}>
-                        <h3>CMS構築</h3>
-                        <p>CMS構築承ります。WordPressをメインに構築しています。気が付けばWordPressの使用経験は10年近くなっていました。お知らせを更新できるサイトが欲しい、お知らせの他にもブログ投稿のページが欲しいなどご要望に応じてカスタムいたします。WordPressの他にもNext.jsとヘッドレスCMSを使用したJamstackも経験あります。</p>
+                        <h3>WordPressを構築</h3>
+                        <p>WordPressの構築を行っています。既存テーマのカスタマイズからオリジナルテーマの開発まで経験があります。本ポートフォリオはヘッドレスCMSのmicroCMSを利用しています。</p>
                     </div>
                 </div>
             </div>
